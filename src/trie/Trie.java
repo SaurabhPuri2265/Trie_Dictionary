00c95135ac04eb5967c09b93add99ba2e6ccd818
SaurabhPuri2265 <47772384+SaurabhPuri2265@users.noreply.github.com>
2020-08-22 12:24:36 -0700

    23-08-2020

--- src/trie/Trie.java
+++ src/trie/Trie.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package trie;
 import java.util.*;
 
@@ -15,10 +11,7 @@ import java.nio.file.*;
 import java.io.*;
 
 
-/**
- *
- * @author Dell
- */
+
 public class Trie {
      
     int wordSize=40;
